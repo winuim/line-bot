adf8acc2de2e42800defb3e950c9cfa3bac4df68
Uema Yohei <winuim@gmail.com>
2020-03-04 14:42:09 +0900

    kitchensink同等に機能追加

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,4 +1,5 @@
 import * as shell from "shelljs";
 
 shell.mkdir("-p", "dist/public/");
+shell.mkdir("-p", "dist/downloaded/");
 shell.cp("-R", "src/public/*", "dist/public/");
--- src/controllers/callback.ts
+++ src/controllers/callback.ts
@@ -3,9 +3,16 @@ import {
   EventSource,
   TextMessage,
   WebhookEvent,
-  Message
+  LocationEventMessage,
+  StickerEventMessage,
+  ImageEventMessage,
+  VideoEventMessage,
+  AudioEventMessage
 } from "@line/bot-sdk";
+import cp from "child_process";
 import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 import botText from "../config/botText.json";
 type BotText = typeof botText;
@@ -14,7 +21,8 @@ type botTextKey = keyof BotText;
 // create LINE SDK config from env variables
 export const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
+  channelSecret: process.env.CHANNEL_SECRET,
+  baseURL: process.env.BASE_URL
 };
 
 // create LINE SDK client
@@ -28,6 +36,24 @@ export const replyText = (token: string, texts: string | string[]) => {
     texts.map(text => ({ type: "text", text }))
   );
 };
+
+// content download
+function downloadContent(messageId: string, downloadPath: string) {
+  return client
+    .getMessageContent(messageId)
+    .then(
+      stream =>
+        new Promise((resolve, reject) => {
+          const writable = fs.createWriteStream(downloadPath);
+          stream.pipe(writable);
+          stream.on("end", () => resolve(downloadPath));
+          stream.on("error", reject);
+        })
+    )
+    .then(() => {
+      return downloadPath;
+    });
+}
 
 export function handleText(
   message: TextMessage,
@@ -53,17 +79,170 @@ export function handleText(
         );
       }
     }
+    case "bye": {
+      switch (source.type) {
+        case "user": {
+          return replyText(replyToken, "Bot can't leave from 1:1 chat");
+        }
+        case "group": {
+          return replyText(replyToken, "Leaving group").then(() =>
+            client.leaveGroup(source.groupId)
+          );
+        }
+        case "room": {
+          return replyText(replyToken, "Leaving room").then(() =>
+            client.leaveRoom(source.roomId)
+          );
+        }
+      }
+      break;
+    }
     default: {
       const textKey = message.text;
       if (textKey in botText) {
-        const msg = botText[textKey as botTextKey] as Message;
-        return client.replyMessage(replyToken, msg);
+        const responseString = JSON.stringify(botText[textKey as botTextKey]);
+        const msg = JSON.parse(
+          responseString.replace(/\$BASE_URL/g, config.baseURL)
+        );
+        return client.replyMessage(replyToken, msg as TextMessage);
       } else {
         console.log(`Echo message to ${replyToken}: ${message.text}`);
         return replyText(replyToken, message.text);
       }
     }
   }
+}
+
+function handleImage(message: ImageEventMessage, replyToken: string) {
+  let getContent;
+  if (message.contentProvider.type === "line") {
+    const downloadPath = path.join(
+      __dirname,
+      "dist/downloaded",
+      `${message.id}.jpg`
+    );
+    const previewPath = path.join(
+      __dirname,
+      "dist/downloaded",
+      `${message.id}-preview.jpg`
+    );
+
+    getContent = downloadContent(message.id, downloadPath).then(
+      downloadPath => {
+        // ImageMagick is needed here to run 'convert'
+        // Please consider about security and performance by yourself
+        cp.execSync(
+          `convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`
+        );
+
+        return {
+          originalContentUrl:
+            config.baseURL + "/downloaded/" + path.basename(downloadPath),
+          previewImageUrl:
+            config.baseURL + "/downloaded/" + path.basename(previewPath)
+        };
+      }
+    );
+  } else if (message.contentProvider.type === "external") {
+    getContent = Promise.resolve(message.contentProvider);
+  }
+
+  return getContent.then(({ originalContentUrl, previewImageUrl }) => {
+    return client.replyMessage(replyToken, {
+      type: "image",
+      originalContentUrl,
+      previewImageUrl
+    });
+  });
+}
+
+function handleVideo(message: VideoEventMessage, replyToken: string) {
+  let getContent;
+  if (message.contentProvider.type === "line") {
+    const downloadPath = path.join(
+      __dirname,
+      "dist/public/downloaded",
+      `${message.id}.mp4`
+    );
+    const previewPath = path.join(
+      __dirname,
+      "dist/public/downloaded",
+      `${message.id}-preview.jpg`
+    );
+
+    getContent = downloadContent(message.id, downloadPath).then(
+      downloadPath => {
+        // FFmpeg and ImageMagick is needed here to run 'convert'
+        // Please consider about security and performance by yourself
+        cp.execSync(`convert mp4:${downloadPath}[0] jpeg:${previewPath}`);
+
+        return {
+          originalContentUrl:
+            config.baseURL + "/downloaded/" + path.basename(downloadPath),
+          previewImageUrl:
+            config.baseURL + "/downloaded/" + path.basename(previewPath)
+        };
+      }
+    );
+  } else if (message.contentProvider.type === "external") {
+    getContent = Promise.resolve(message.contentProvider);
+  }
+
+  return getContent.then(({ originalContentUrl, previewImageUrl }) => {
+    return client.replyMessage(replyToken, {
+      type: "video",
+      originalContentUrl,
+      previewImageUrl
+    });
+  });
+}
+
+function handleAudio(message: AudioEventMessage, replyToken: string) {
+  let getContent;
+  if (message.contentProvider.type === "line") {
+    const downloadPath = path.join(
+      __dirname,
+      "dist/public/downloaded",
+      `${message.id}.m4a`
+    );
+
+    getContent = downloadContent(message.id, downloadPath).then(
+      downloadPath => {
+        return {
+          originalContentUrl:
+            config.baseURL + "/downloaded/" + path.basename(downloadPath)
+        };
+      }
+    );
+  } else {
+    getContent = Promise.resolve(message.contentProvider);
+  }
+
+  return getContent.then(({ originalContentUrl }) => {
+    return client.replyMessage(replyToken, {
+      type: "audio",
+      originalContentUrl,
+      duration: message.duration
+    });
+  });
+}
+
+function handleLocation(message: LocationEventMessage, replyToken: string) {
+  return client.replyMessage(replyToken, {
+    type: "location",
+    title: message.title,
+    address: message.address,
+    latitude: message.latitude,
+    longitude: message.longitude
+  });
+}
+
+function handleSticker(message: StickerEventMessage, replyToken: string) {
+  return client.replyMessage(replyToken, {
+    type: "sticker",
+    packageId: message.packageId,
+    stickerId: message.stickerId
+  });
 }
 
 // event handler
@@ -82,9 +261,41 @@ export function handleEvent(event: WebhookEvent) {
       switch (message.type) {
         case "text":
           return handleText(message, event.replyToken, event.source);
+        case "image":
+          return handleImage(message, event.replyToken);
+        case "video":
+          return handleVideo(message, event.replyToken);
+        case "audio":
+          return handleAudio(message, event.replyToken);
+        case "location":
+          return handleLocation(message, event.replyToken);
+        case "sticker":
+          return handleSticker(message, event.replyToken);
         default:
           throw new Error(`Unknown message: ${JSON.stringify(message)}`);
       }
+    }
+    case "follow": {
+      return replyText(event.replyToken, "Got followed event");
+    }
+    case "unfollow": {
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+    }
+    case "join": {
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+    }
+    case "leave": {
+      return console.log(`Left: ${JSON.stringify(event)}`);
+    }
+    case "postback": {
+      let data = event.postback.data;
+      if (data === "DATE" || data === "TIME" || data === "DATETIME") {
+        data += `(${JSON.stringify(event.postback.params)})`;
+      }
+      return replyText(event.replyToken, `Got postback: ${data}`);
+    }
+    case "beacon": {
+      return replyText(event.replyToken, `Got beacon: ${event.beacon.hwid}`);
     }
     default: {
       throw new Error(`Unknown event: ${JSON.stringify(event)}`);
